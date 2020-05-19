35f9640dac587d5e0c560824effd11bcd5f19b3e
zhengmenghuang <1481010383@qq.com>
2020-05-19 12:06:07 +0800

    bug 修复提示

--- src/app/services/others/can-deactivate/can-deactivate.guard.ts
+++ src/app/services/others/can-deactivate/can-deactivate.guard.ts
@@ -8,6 +8,11 @@ import {CanActivateComponent} from '../../../pages/can-activate/can-activate.com
 })
 export class CanDeactivateGuard implements CanDeactivate<CanActivateComponent> {
   canDeactivate(component: CanActivateComponent) {
+    /**
+     * 这里有angular的bug
+     * 页面虽然停留在了当前 但是history已经改变 所以当点了取消后需要重新导航到当前url
+     * this.location.go(this.router.createUrlTree([], this.route.snapshot).toString());
+     */
     return component.canDeactivate ? component.canDeactivate() : true;
   }
 }
